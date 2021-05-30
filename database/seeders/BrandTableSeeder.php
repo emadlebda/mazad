b425d86dcf7949338e6e9cf582cadb90d55ca342
Emad A. Abu Lebda <emadexodus@gmail.com>
2021-05-30 15:29:57 +0300

    updating

--- database/seeders/BrandTableSeeder.php
+++ database/seeders/BrandTableSeeder.php
@@ -18,71 +18,111 @@ class BrandTableSeeder extends Seeder
             [
                 'id' => 1,
                 'title' => 'Mercedes-Benz',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 2,
                 'title' => 'BMW',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 3,
                 'title' => 'VW',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 4,
                 'title' => 'Alfa Romeo',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 5,
                 'title' => 'Aston Martin',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 6,
                 'title' => 'Bentley',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 7,
                 'title' => 'Cadillac',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 8,
                 'title' => 'Chevrolet',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 9,
                 'title' => 'Dodge',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 10,
                 'title' => 'Ferrari',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 11,
                 'title' => 'FIAT',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 12,
                 'title' => 'Ford',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 13,
                 'title' => 'GMC',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 14,
                 'title' => 'Honda',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 15,
                 'title' => 'Hyundai',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 16,
                 'title' => 'Jaguar',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 17,
                 'title' => 'Jeep',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 18,
                 'title' => 'KIA',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 19,
                 'title' => 'Maserati',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 20,
                 'title' => 'Mazda',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
         ];
 
--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -18,35 +18,55 @@ class CategoryTableSeeder extends Seeder
             [
                 'id' => 1,
                 'title' => 'SUV',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 2,
                 'title' => 'pickup Truck',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
             [
                 'id' => 3,
                 'title' => 'sedan',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 4,
                 'title' => 'coupe',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 5,
                 'title' => 'crossover',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 6,
                 'title' => 'hatchback',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 7,
                 'title' => 'hybrid',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 8,
                 'title' => 'convertible',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 9,
                 'title' => 'minivan/van',
+                'created_at' => now(),
+                'updated_at' => now(),
             ], [
                 'id' => 10,
                 'title' => 'Luxury',
+                'created_at' => now(),
+                'updated_at' => now(),
             ],
         ];
 
