644e929597d0beef317ed1cce261fd64a79c6c9f
unknown <hashemhassan99>
2021-08-25 17:21:59 +0300

    edit kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckBanned' => \App\Http\Middleware\CheckBannedMiddleware::class,
+        'csrf' => \Http\Middleware\VerifyCsrfToken::class,
     ];
 
     protected $middlewareGroups = [
